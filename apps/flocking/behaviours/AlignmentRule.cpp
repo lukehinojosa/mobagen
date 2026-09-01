58e69c16adc6b4276364cdfb3990c5d8e400764a
lukehinojosa <lukehinojosa05@gmail.com>
2026-09-01 15:08:38 -0400

    Alignment rule completed and passes tests

--- apps/flocking/behaviours/AlignmentRule.cpp
+++ apps/flocking/behaviours/AlignmentRule.cpp
@@ -6,7 +6,12 @@ glm::vec2 AlignmentRule::computeForce(const std::vector<BoidView>& neighborhood,
   // glm::vec2 can be divided by a float, which will divide each component of the vector by that float.
 
   // begin solution
+  for (int i = 0; i < neighborhood.size(); i++) {
+    averageVelocity += neighborhood[i].velocity;
+  }
 
+  if (neighborhood.size() > 0)
+    averageVelocity /= neighborhood.size();
 
   return averageVelocity;
   // end solution
