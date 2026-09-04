0ffe00715c5ab73087e5dfe445953e106c6c9615
lukehinojosa <lukehinojosa05@gmail.com>
2026-09-04 13:55:20 -0400

    Added Wind force

--- apps/flocking/behaviours/WindRule.cpp
+++ apps/flocking/behaviours/WindRule.cpp
@@ -7,7 +7,7 @@ glm::vec2 WindRule::computeForce(const std::vector<BoidView>& neighborhood, cons
   // windAngle is the angle of the wind direction in degrees, so we need to convert it to radians by multiplying it by (pi / 180).
   
   // begin solution
-  return glm::vec2(0,0);
+  return {cos(windAngle),sin(windAngle)};
   // end solution
 }
 
